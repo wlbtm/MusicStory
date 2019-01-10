13da53343bd8f23d8d38bb9735179e57844078db
ngcly <531237716@qq.com>
2019-01-10 14:58:59 +0800

    添加部分轮播图管理页面

--- commons/src/main/java/com/cn/util/UploadUtil.java
+++ commons/src/main/java/com/cn/util/UploadUtil.java
@@ -19,15 +19,16 @@ import java.io.IOException;
 public class UploadUtil {
 
     /**
-     * 阿里云基本信息
+     * 云基本信息
      */
     private static String endpoint = "http://oss-cn-hongkong-internal.aliyuncs.com";
-    private static String accessKeyId = "<yourAccessKeyId>";
-    private static String accessKeySecret = "<yourAccessKeySecret>";
+    private static String accessKey = "LTAI6bpHNc1Fjfb7";
+    private static String secretKey = "KBH0GfDdJAhTyMOZ3kCXqBmVtNmueb";
     private static String bucketName = "music-story";
+    private static String returnPath = "http://music-story.oss-cn-hongkong-internal.aliyuncs.com/";
 
     /**
-     * 七牛云上传
+     * 七牛云上传（由于七牛云取消了测试域名，所以用阿里云）
      * @param file
      * @return
      * @throws Exception
@@ -36,16 +37,11 @@ public class UploadUtil {
         //构造一个带指定Zone对象的配置类 七牛上传
         Configuration cfg = new Configuration(Zone.zone2());
         UploadManager uploadManager = new UploadManager(cfg);
-        //...生成上传凭证，然后准备上传
-        String accessKey = "yAbBqIQ633g5U4BtZtEJtGkic8l6ALkngpfNeKX_";
-        String secretKey = "ry0PDc4GZk9Gd6sYpOYg0EGchKdo3S1aPhJJbAPP";
-        String bucket = "music-story";
-        String returnPath="http://p6wg9ob78.bkt.clouddn.com/";
         //默认不指定key的情况下，以文件内容的hash值作为文件名
         String key = null;
         byte[] uploadBytes = file.getBytes();
         Auth auth = Auth.create(accessKey, secretKey);
-        String upToken = auth.uploadToken(bucket);
+        String upToken = auth.uploadToken(bucketName);
 
         Response response = uploadManager.put(uploadBytes, key, upToken);
         //解析上传成功的结果
@@ -62,11 +58,11 @@ public class UploadUtil {
      * @throws IOException
      */
     public static String uploadFileByAli(MultipartFile file,String cloudDir) throws IOException {
-        OSSClient ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret);
+        OSSClient ossClient = new OSSClient(endpoint, accessKey, secretKey);
         // 上传内容到指定的存储空间（bucketName）并保存为指定的文件名称（objectName）。
         String fileKey = cloudDir+"/"+file.getName();
         ossClient.putObject(bucketName, fileKey, new ByteArrayInputStream(file.getBytes()));
-        String url = "http://music-story.oss-cn-hongkong-internal.aliyuncs.com/"+file.getName();
+        String url = returnPath+file.getName();
         // 关闭OSSClient。
         ossClient.shutdown();
         return url;
@@ -74,12 +70,13 @@ public class UploadUtil {
 
     /**
      * 阿里云删除文件
-     * @param fileName
+     * @param url 图片地址
      */
-    public static void deleteFileByAli(String fileName){
+    public static void deleteFileByAli(String url){
         // 创建OSSClient实例。
-        OSSClient ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret);
+        OSSClient ossClient = new OSSClient(endpoint, accessKey, secretKey);
         // 删除文件。
+        String fileName = url.replace(returnPath,"");
         ossClient.deleteObject(bucketName, fileName);
         // 关闭OSSClient。
         ossClient.shutdown();
